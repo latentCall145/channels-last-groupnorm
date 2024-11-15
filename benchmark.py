561e9da9873e01dc82e79af7344be764c3814e7b
Thai-Hoa Nguyen <tonanhhoaduc@gmail.com>
2024-11-14 22:58:49 -0500

    add testing for nhwc torch GN

--- benchmark.py
+++ benchmark.py
@@ -19,8 +19,7 @@ def config_filter(x): # returns true if config is valid
     if R == 1: # this causes an autograd problem where it gets confused since the tensor is both contiguous in NCHW/NHWC format 
         return False
 
-    dtype_size = {torch.half: 2, torch.bfloat16: 2, torch.float: 4, torch.double: 8}[DTYPE] # only care about 16/32-bit dtypes for now
-    #estimated_mem_usage_gib = (25 * dtype_size * B * C * R * R) / 2**30 #  this is just a rough estimate, likely wrong
+    dtype_size = torch.finfo(DTYPE).bits / 8
     estimated_mem_usage_gib = (5 * dtype_size * B * C * R * R) / 2**30 #  this is just a rough estimate, likely wrong
     if estimated_mem_usage_gib > 3: # vram filter
         return False
@@ -38,7 +37,7 @@ if __name__ == '__main__':
     }[ACT_FN]
 
     NSEC = 1 # number of seconds that each kernel runs for on a certain input
-    DTYPES = [torch.bfloat16]
+    DTYPES = [torch.float, torch.bfloat16]
 
     BATCHES = [1, 8]
     CHANNELS = [32, 128]
@@ -47,82 +46,73 @@ if __name__ == '__main__':
 
     GN_KERNELS = [
         (nn.GroupNorm, 'torch.nn GN NCHW'),
+        (nn.GroupNorm, 'torch.nn GN NHWC'),
         (GN_NHWC, 'GN NHWC'),
     ]
 
     os.makedirs('csvs', exist_ok=True)
-    fname = datetime.datetime.now().strftime("csvs/%H-%M-%S-%d-%m-%Y.csv")
+    fname = datetime.datetime.now().strftime(os.path.join('csvs', '%Y-%m-%d-%H-%M-%S.csv'))
     print(f'Writing to {fname}')
     outfile = open(fname, 'w')
-    outfile.write('Kernel,B (batch),C (num channels),R (resolution),G (num groups), D (C/G),Speed (it/s; 25th percentile),Speed (it/s; 50th percentile),Speed (it/s; 75th percentile)\n')
+    outfile.write('Kernel,B (batch),C (num channels),R (resolution),G (num groups), D (C/G),Bandwidth (GB/s),Speed (it/s)\n')
     
     configs = list(filter(config_filter, itertools.product(DTYPES, BATCHES, CHANNELS, RESOLUTIONS, NUM_GROUPS)))
     print('Estimated time (seconds) to complete:', NSEC * len(configs) * len(GN_KERNELS))
+    print(f'Include bwd pass: {INCLUDE_BWD}')
 
     for DTYPE, B, C, R, G in configs:
         x_nchw = torch.randn((B, C, R, R), dtype=DTYPE, device='cuda', requires_grad=True)
         x_nhwc = x_nchw.contiguous(memory_format=torch.channels_last).detach().requires_grad_(True)
 
-        print(blue(f'benchmark (include bwd: {INCLUDE_BWD}) | DTYPE: {DTYPE} | B: {B} | C: {C} | R: {R} | G: {G}'))
+        print(blue(f'DTYPE: {DTYPE} | B: {B} | C: {C} | R: {R} | G: {G}'))
         for gn_class, desc in GN_KERNELS:
+            pad_len = max(len(kernel[1]) for kernel in GN_KERNELS)
+            desc = f'{desc:<{pad_len}}' # right pad desc with spaces
+
             gn_input = x_nchw if 'NCHW' in desc else x_nhwc
             grad = torch.ones_like(gn_input)
-            print(f'\t{desc}')
 
             try:
                 gn_layer = gn_class(G, C).to(DTYPE).cuda()
 
                 graph = torch.cuda.CUDAGraph()
-                with torch.cuda.graph(graph):
+                with torch.cuda.graph(graph): # using CUDA graphs to reduce CPU overhead
                     g = gn_layer(gn_input)
                     if not isinstance(gn_layer, GN_NHWC):
                         g = act_fn(g)
                     if INCLUDE_BWD:
-                        g.backward(grad)
+                        torch.autograd.grad(g, gn_input, grad_outputs=grad, retain_graph=True)
                 torch.cuda.synchronize()
 
                 tic = time.perf_counter()
                 tic_sec = time.perf_counter()
-                ntrials = ntrials_minor = 0
-                minor_speeds = [] # used to track speed percentiles since they can often vary by a lot
+                ntrials = 0
 
                 while time.perf_counter() - tic < NSEC:
                     graph.replay()
-                    #g = gn_layer(gn_input)
-                    #if not isinstance(gn_layer, GN_NHWC):
-                    #    g = act_fn(g)
-                    #if BENCH != 'fwd':
-                    #    g.backward(grad)
-
                     torch.cuda.synchronize()
                     ntrials += 1
-                    ntrials_minor += 1
 
                     if time.perf_counter() - tic_sec > 0.1:
-                        speed = round(ntrials_minor / (time.perf_counter() - tic_sec), 2)
-                        minor_speeds.append(speed)
-
-                        bw1 = gn_input.numel() * (3+5 if INCLUDE_BWD else 3) * {torch.half:2,torch.bfloat16:2, torch.float:4,torch.double:8}[DTYPE]
+                        speed = round(ntrials / (time.perf_counter() - tic), 2)
                         bw = ntrials * gn_input.nbytes * (3+5 if INCLUDE_BWD else 3) / (time.perf_counter() - tic)
-                        print(f'\t\tduration: {time.perf_counter() - tic:.1f}/{NSEC} seconds completed, bandwidth: {blue(round(bw / 1e9, 2))} GB/s, speed: {blue(speed)} it/s           \r', end='')
-                        ntrials_minor = 0
+                        bw = round(bw / 1e9, 2)
+                        print(f'\t{desc} | {time.perf_counter() - tic:.1f}/{NSEC} sec, bandwidth: {green(bw)} GB/s, speed: {yellow(speed)} it/s           \r', end='')
                         tic_sec = time.perf_counter()
 
-                minor_speeds = np.array(minor_speeds)
-                median_speed = round(np.percentile(minor_speeds, 50), 2)
-                slow_speed = round(np.percentile(minor_speeds, 25), 2)
-                fast_speed = round(np.percentile(minor_speeds, 75), 2)
-                print(f'\n\t\tSpeed (25th/50th/75th percentile): {red(slow_speed)}/{yellow(median_speed)}/{green(fast_speed)} it/s')
+                speed = round(ntrials / (time.perf_counter() - tic), 2)
+                bw = ntrials * gn_input.nbytes * (3+5 if INCLUDE_BWD else 3) / (time.perf_counter() - tic)
+                bw = round(bw / 1e9, 2)
+                print(f'\t{desc} | bandwidth: {green(bw)} GB/s, speed: {yellow(speed)} it/s                             ')
             except KeyboardInterrupt:
-                print(f'Keyboard interrupt, closing {fname}.')
+                print(f'Keyboard interrupt, closing {fname}                               ')
                 outfile.close()
                 raise
             except Exception as e:
-                print('\t\tFAILED; Error:', str(e).strip())
+                print(red(f'\t{desc} | FAILED; err msg:'), str(e).strip())
                 raise
-                median_speed = slow_speed = fast_speed = '-1 (failed)'
+                speed = '-1 (failed)'
             
-            outfile.write(f'{desc},{B},{C},{R},{G},{C//G},{slow_speed},{median_speed},{fast_speed}\n')
-        print()
-    print(f'All tests done, closing {fname}.')
+            outfile.write(f'{desc},{B},{C},{R},{G},{C//G},{bw},{speed}\n')
+    print(f'All tests done, closing {fname}')
     outfile.close()
--- prof.py
+++ prof.py
@@ -17,19 +17,23 @@ if __name__ == '__main__':
     }[ACT_FN]
 
     inputs = [
-        (torch.half, 2, 512, 128, 128, 32),
+        (torch.half, 8, 32, 512, 512, 32),
     ]
 
     for DTYPE, B, C, H, W, G in tqdm(inputs):
         print(f'profiling | DTYPE: {DTYPE} |B: {B:<2} | C: {C:<4} | H: {H:<4} | W: {W:<4} | G: {G:<3}')
-        x = torch.randn((B, C, H, W), dtype=DTYPE, device='cuda')
+        x = torch.empty((B, C, H, W), dtype=DTYPE, device='cuda')
         dy = torch.ones_like(x)
         x_nhwc = x.to(memory_format=torch.channels_last)
         dy_nhwc = dy.to(memory_format=torch.channels_last)
         x.requires_grad_(True)
         x_nhwc.requires_grad_(True)
 
-        gn = GN_NHWC(G, C, activation=ACT_FN).cuda().to(DTYPE)
-        gn(x_nhwc).backward(dy_nhwc)
-        gn_ref = nn.GroupNorm(G, C).cuda().to(DTYPE)
-        gn_ref(x).backward(dy)
+        custom_gn = GN_NHWC(G, C, activation=ACT_FN).cuda().to(DTYPE)
+        custom_gn(x_nhwc).backward(dy_nhwc)
+
+        gn_ref_nhwc = nn.GroupNorm(G, C).cuda().to(DTYPE)
+        gn_ref_nhwc(x_nhwc).backward(dy_nhwc)
+
+        gn_ref_nchw = nn.GroupNorm(G, C).cuda().to(DTYPE)
+        gn_ref_nchw(x).backward(dy)
