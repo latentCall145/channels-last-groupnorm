1ac263db589f901c7037441c41d631bfbc89636c
Thai-Hoa Nguyen <tonanhhoaduc@gmail.com>
2024-11-14 23:01:47 -0500

    added more tests (checking grads, weird mem formats)

--- test/nchw_nhwc_equal.py
+++ test/nchw_nhwc_equal.py
@@ -1,14 +1,25 @@
 # Checks if the outputs between NCHW and NHWC inputs are the same for the GN_NHWC layer
 # note: this is not an exhaustive check
 from gnnhwc import GN_NHWC
+import torch.nn as nn
 import torch
 
 def test_nchw_nwhc():
     N, R, G, C = 1, 256, 32, 128
     x = torch.randn((N, C, R, R)).cuda()
     x_nhwc = x.to(memory_format=torch.channels_last)
+    x.requires_grad_(True)
+    x_nhwc.requires_grad_(True)
+    rand_dy = torch.rand_like(x)
+    rand_dy_nhwc = rand_dy.to(memory_format=torch.channels_last)
 
     for act in ['identity', 'silu', 'gelu', 'gelu_tanh']:
         for dtype in [torch.half, torch.float, torch.double, torch.bfloat16]:
             m = GN_NHWC(G, C, act).cuda().to(dtype)
-            assert (m(x.to(dtype)) - m(x_nhwc.to(dtype))).square().mean() < 1e-6
+            #m = nn.GroupNorm(G, C).cuda().to(dtype)
+            out1 = m(x.to(dtype))
+            out2 = m(x_nhwc.to(dtype))
+            out1.backward(rand_dy)
+            out2.backward(rand_dy_nhwc)
+            assert (out1 - out2).square().mean() < 1e-6
+            assert (x.grad - x_nhwc.grad).square().mean() < 1e-6
--- test/trailing_dims.py
+++ test/trailing_dims.py
@@ -11,12 +11,14 @@ def test_trailing_dims():
         (1, 256, [7, 13, 19, 1]),
         (1, 128, [32, 1, 1, 1]),
         (1,  32, [32, 128, 32, 1]),
+
     ]:
         x = torch.randn((N, *R_list, C)).cuda()
-        x = x.view(N, -1, C).permute(0, 2, 1).view(N, C, *R_list) # hack to get channels_last behavior for non-4D tensors
+        x = x.permute(0, -1, *range(1, len(R_list)+1)) # get channels-last-like behavior (namely x.stride(1) = 1) for tensors with ndim > 4
         G = 32
         for dtype in [torch.half, torch.float]:
             m = GN_NHWC(G, C).cuda().to(dtype)
+            #m = nn.GroupNorm(G, C).cuda().to(dtype)
             m_ref = nn.GroupNorm(G, C).cuda().to(dtype)
 
             y = m(x.to(dtype))
@@ -29,3 +31,41 @@ def test_trailing_dims():
                 x = x.view(N, C, *R_list)
                 y = m(x.to(dtype))
                 assert F.mse_loss(y_ref.view(N, C, *R_list), y) < 1e-6
+
+def test_weird_mem_fmt():
+    B = 2
+    C = 32
+    G = 8
+    '''
+    test pseudo-channels last (x.stride(1) = 1 but non-channels-last) and pseudo-contiguous (x.stride(-1) = 1 but non-contiguous) inputs
+    example: x.shape = (5, 4, 3, 2)
+    contiguous:           x.stride = (24, 6, 2, 1)
+    pseudo-contiguous:    x.stride = (24, 2, 8, 1)
+    channels-last:        x.stride = (24, 1, 8, 4)
+    pseudo-channels-last: x.stride = (24, 1, 4, 12)
+    '''
+    for dtype in [torch.half, torch.float]:
+        m = GN_NHWC(G, C).cuda().to(dtype)
+        #m = nn.GroupNorm(G, C).cuda().to(dtype)
+        m_ref = nn.GroupNorm(G, C).cuda().to(dtype)
+
+        for inner_dims in (
+            (3, 5),
+            (3, 5, 7),
+            (3, 5, 7, 11),
+        ):
+            # pseudo-channels-last (x.stride(1) == 1)
+            x = torch.randn((B, *inner_dims, C), device='cuda')
+            x = x.permute(0, -1, *range(len(inner_dims), 0, -1))
+
+            y = m(x.to(dtype))
+            y_ref = m_ref(x.to(dtype))
+            assert F.mse_loss(y, y_ref) < 1e-6 # make sure GN is consistent to the reference
+
+            # pseudo-contiguous (x.stride(-1) == 1)
+            x = torch.randn((B, C, *inner_dims), device='cuda')
+            x = x.permute(0, 1, *range(len(inner_dims), 1, -1), -1)
+
+            y = m(x.to(dtype))
+            y_ref = m_ref(x.to(dtype))
+            assert F.mse_loss(y, y_ref) < 1e-6 # make sure GN is consistent to the reference
